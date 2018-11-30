892d1c7f4cf4db95ad6a5aa808044d163242cf5b
Braden Shipley <bradenshipley@Kaycees-MacBook-Air.local>
2018-11-30 08:28:18 -0600

    add solutions

--- src/components/EditToggle.js
+++ src/components/EditToggle.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React from "react";
 
-export default function EditToggle() {
+export default function EditToggle(props) {
   return (
-    <select className="dropDownContainer ml0">
+    <select
+      className="dropDownContainer ml0"
+      onChange={e => props.update(e.target.value)}
+      disabled={props.allowEdit === "false"}
+    >
       <option value="true"> Allow Edit </option>
       <option value="false"> Disable Edit </option>
     </select>
-  )
+  );
 }
